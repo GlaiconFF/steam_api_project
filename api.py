0cbb9eb0a0aae05a143f1c3d49ba1be482353cfe
Glaicon <glaiconffelipe@gmail.com>
2026-05-12 21:27:34 -0300

    Added language change

--- api.py
+++ api.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from scraper.steam_api import choose_game, search_game
 
 app = Flask(__name__)
 
+app.secret_key = "password"
+
+currencies = {"english": "en", "brazilian": "br", "spanish": "es"}
+
+translations = {"english": {
+                    "search_placeholder": "Search game",
+                    "search_button": "Search",
+                    "free_game": "Free to Play"
+                },
+                "brazilian": {
+                    "search_placeholder": "Pesquisar jogo",
+                    "search_button": "Pesquisar",
+                    "free_game": "Gratuito para Jogar"
+                },
+                "spanish": {
+                    "search_placeholder": "Buscar juego",
+                    "search_button": "Buscar",
+                    "free_game": "Free to Play"
+                }
+}
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.context_processor
+def inject_translation():
+
+    language = session.get("language", "english")
+
+    return {
+        "translation": translations[language]
+    }
 
 @app.route("/search")
 def search():
@@ -15,8 +45,21 @@ def search():
 
 @app.route("/show_game/<id>")
 def show_game(id):
-    game_data = search_game(id)
-    return render_template("index.html", game_data=game_data)
+
+    language = session.get("language", "english")
+    currency = currencies[language]
+    translation = translations[language]
+
+    game_data = search_game(id, language, currency)
+    
+    return render_template("index.html", game_data=game_data, translation=translation)
+
+@app.route("/set_language/<language>")
+def set_language(language):
+
+    session["language"] = language
+
+    return redirect(request.referrer)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scraper/steam_api.py
+++ scraper/steam_api.py
@@ -20,15 +20,15 @@ def choose_game(name):
 
     return games_images
 
-def search_game(game_id):
-    game_reviews_response = session.get(id_reviews+f"{game_id}?json=1&filter=all&language=all&l=en")
+def search_game(game_id, language, currency):
+    game_reviews_response = session.get(id_reviews+f"{game_id}?json=1&filter=all&language=all&l={language}")
     game_reviews_data = game_reviews_response.json()
 
     game_positive_reviews = game_reviews_data['query_summary'].get('total_positive')
     game_negative_reviews = game_reviews_data['query_summary'].get('total_negative')
     game_total_reviews = game_reviews_data['query_summary'].get('total_reviews')
 
-    game_details_response = session.get(item_details+f"?appids={game_id}&cc=en&l=en")
+    game_details_response = session.get(item_details+f"?appids={game_id}&cc={currency}&l={language}")
     game_data = game_details_response.json()
     game_header_image = game_data[game_id]['data'].get('header_image')
     game_name = game_data[game_id]['data'].get('name')
@@ -37,6 +37,7 @@ def search_game(game_id):
     game_price = game_data[game_id]['data'].get('price_overview', {}).get('final_formatted', {})
     game_not_launched = game_data[game_id]['data']['release_date'].get('coming_soon')
     game_release_date = game_data[game_id]['data']['release_date'].get('date', 0)
+    game_supported_languages = game_data[game_id]['data'].get('supported_languages').replace('*','')
 
     game_dict = {
         "header_image": game_header_image,
@@ -46,6 +47,7 @@ def search_game(game_id):
         "price": game_price,
         "not_launched": game_not_launched,
         "release_date": game_release_date,
+        "supported_languages": game_supported_languages,
         "positive_reviews": game_positive_reviews,
         "negative_reviews": game_negative_reviews,
         "total_reviews": game_total_reviews
